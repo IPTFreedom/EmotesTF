d12355a1d3578a9e0db2f15b13c0ba8b9a1b3133
Luna <lunahatesgogle@gmail.com>
2023-06-08 20:12:20 -0300

    Bump version

--- src/main/java/me/mafrans/emotestf/util/FileUtils.java
+++ src/main/java/me/mafrans/emotestf/util/FileUtils.java
@@ -1,7 +1,6 @@
 package me.mafrans.emotestf.util;
 
 import java.io.*;
-import java.net.URL;
 
 public class FileUtils {
     public static String readFile(final File file) throws IOException {
@@ -18,23 +17,5 @@ public class FileUtils {
         }
 
         return var6;
-    }
-
-    @Deprecated
-    public static String readStream(final InputStream stream) throws IOException {
-        final BufferedInputStream in = new BufferedInputStream(stream);
-        final byte[] contents = new byte[1024];
-        int bytesRead;
-        final StringBuilder strFileContents = new StringBuilder();
-
-        while ((bytesRead = in.read(contents)) != 0) {
-            strFileContents.append(new String(contents, 0, bytesRead));
-        }
-
-        return strFileContents.toString();
-    }
-
-    public static void copyFile(final URL origin, final File file) throws IOException {
-        org.apache.commons.io.FileUtils.copyURLToFile(origin, file);
     }
 }
